feffabb2d4a4508467dae4d2a5b28afedb85c6a7
Yangcunhzang <1023592465@qq.com>
2018-01-14 15:47:33 +0800

    添加了cookie,保存在session级别 HttpServletResponse可以进行jsp页面跳转resp.sendRedirect("xxxx.jsp");

--- src/main/java/ServletDemo/servletDemo.java
+++ src/main/java/ServletDemo/servletDemo.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.Writer;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,20 +16,30 @@ public class servletDemo extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println(req.getParameter("username"));
 		System.out.println(req.getParameter("password"));
-	
+
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
-		
+
 		resp.setContentType("text/html;charset=UTF-8");
 		Writer webWriter = resp.getWriter();
-		
+
 		webWriter.write("<html>");
 		webWriter.write("<body>");
-		webWriter.write("<p>"+username+"</p>");
-		webWriter.write("<p>"+password+"</p>");
+		webWriter.write("<p>" + username + "</p>");
+		webWriter.write("<p>" + password + "</p>");
 		webWriter.write("</body>");
 		webWriter.write("</html>");
+		//cookie保存在session中
+		Cookie ucookie = new Cookie("username", username);
+		Cookie pcookie = new Cookie("password", password);
+
+		ucookie.setMaxAge(3600);
+		pcookie.setMaxAge(3600);
+
+		resp.addCookie(ucookie);
+		resp.addCookie(pcookie);
+
+		resp.sendRedirect("readCookie.jsp");
 	}
-	
 
 }
